71ddd223f63dd862450e99f945463cf39ed8bcd4
Sufiyan Saboowala <sufiyansaboowala13@gmail.com>
2020-09-09 07:19:41 -0400

    Updated App and Message Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Input,
+  IconButton,
+} from "@material-ui/core";
 import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
 
 import "./App.css";
 import Message from "./Message";
@@ -15,7 +23,9 @@ function App() {
     db.collection("messages")
       .orderBy("timestamp", "desc")
       .onSnapshot((snapshot) => {
-        setMessages(snapshot.docs.map((doc) => doc.data()));
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
       });
   }, []);
 
@@ -36,35 +46,43 @@ function App() {
 
   return (
     <div className="App">
+      <img
+        style={{ margin: 10 }}
+        src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
+      />
       <h1>
         Welcome {`${username}! `}
         <span role="img" aria-label="rocket-emoji">
           😊
         </span>
       </h1>
-      <form>
-        <FormControl>
+      <form className="app__form">
+        <FormControl className="app__formControl">
           <InputLabel htmlFor="my-input">Enter a message...</InputLabel>
           <Input
+            className="app__formInput"
             id="my-input"
             aria-describedby="my-helper-text"
             value={input}
             onChange={(event) => setInput(event.target.value)}
           />
-          <Button
+          <IconButton
+            className="app__formButton"
             variant="contained"
             color="primary"
             type="submit"
             onClick={handleMessageSend}
             disabled={!input}
           >
-            Send Message
-          </Button>
+            <SendIcon />
+          </IconButton>
         </FormControl>
-        {messages.map((message) => (
-          <Message username={username} message={message} />
-        ))}
       </form>
+      <FlipMove>
+        {messages.map(({ id, message }) => (
+          <Message key={id} username={username} message={message} />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
 import "./Message.css";
 
-const Message = ({ username, message }) => {
+const Message = forwardRef(({ username, message }, ref) => {
   const isUser = username === message.username;
   return (
-    <div className={`message ${isUser && "message_user"}`}>
+    <div ref={ref} className={`message ${isUser && "message_user"}`}>
       <Card style={{ backgroundColor: isUser ? "#0b81ff" : "#e9e9eb" }}>
         <CardContent>
           <Typography variant="h5" component="h2">
-            {message.username} : {message.message}
+            {!isUser && `${message.username || "Anonymous"}: `}{" "}
+            {message.message}
           </Typography>
         </CardContent>
       </Card>
     </div>
   );
-};
+});
 
 export default Message;
